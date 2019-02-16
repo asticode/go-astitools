479b837057ff336c6a7012fcc65def7ff1c9f471
Quentin Renard <contact@asticode.com>
2019-02-16 12:25:02 +0100

    Updated Rational

--- float/rational.go
+++ float/rational.go
@@ -8,20 +8,46 @@ import (
 )
 
 // Rational represents a rational
-type Rational struct{ Den, Num int }
+type Rational struct{ den, num int }
+
+// NewRational creates a new rational
+func NewRational(num, den int) *Rational {
+	return &Rational{
+		den: den,
+		num: num,
+	}
+}
+
+// Num returns the rational num
+func (r *Rational) Num() int {
+	return r.num
+}
+
+// Den returns the rational den
+func (r *Rational) Den() int {
+	return r.den
+}
+
+// ToFloat64 returns the rational as a float64
+func (r *Rational) ToFloat64() float64 {
+	return float64(r.num) / float64(r.den)
+}
 
 // UnmarshalText implements the TextUnmarshaler interface
 func (r *Rational) UnmarshalText(b []byte) (err error) {
-	r.Num = 0
-	r.Den = 1
+	r.num = 0
+	r.den = 1
+	if len(b) == 0 {
+		return
+	}
 	items := bytes.Split(b, []byte("/"))
-	if r.Num, err = strconv.Atoi(string(items[0])); err != nil {
-		err = errors.Wrapf(err, "astiencoder: atoi of %s failed", string(items[0]))
+	if r.num, err = strconv.Atoi(string(items[0])); err != nil {
+		err = errors.Wrapf(err, "astifloat: atoi of %s failed", string(items[0]))
 		return
 	}
 	if len(items) > 1 {
-		if r.Den, err = strconv.Atoi(string(items[1])); err != nil {
-			err = errors.Wrapf(err, "astiencoder: atoi of %s failed", string(items[1]))
+		if r.den, err = strconv.Atoi(string(items[1])); err != nil {
+			err = errors.Wrapf(err, "astifloat: atoi of %s failed", string(items[1]))
 			return
 		}
 	}
