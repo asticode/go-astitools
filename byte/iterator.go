53be41a328d457436c708283183daa3fd1e0d3f3
Quentin Renard <contact@asticode.com>
2019-08-09 11:43:04 +0200

    Added bytes iterator

---
+++ byte/iterator.go
@@ -0,0 +1,68 @@
+package astibyte
+
+import "fmt"
+
+// Iterator represents an object capable of iterating sequentially and safely through a slice of bytes
+type Iterator struct {
+	bs     []byte
+	offset int
+}
+
+// NewIterator creates a new iterator
+func NewIterator(bs []byte) *Iterator {
+	return &Iterator{bs: bs}
+}
+
+// NextByte returns the slice's next byte
+func (i *Iterator) NextByte() (b byte, err error) {
+	if len(i.bs) < i.offset+1 {
+		err = fmt.Errorf("astits: slice length is %d, offset %d is invalid", len(i.bs), i.offset)
+		return
+	}
+	b = i.bs[i.offset]
+	i.offset++
+	return
+}
+
+// NextBytes returns the n slice's next bytes
+func (i *Iterator) NextBytes(n int) (bs []byte, err error) {
+	if len(i.bs) < i.offset+n {
+		err = fmt.Errorf("astits: slice length is %d, offset %d is invalid", len(i.bs), i.offset+n)
+		return
+	}
+	bs = make([]byte, n)
+	copy(bs, i.bs[i.offset:i.offset+n])
+	i.offset += n
+	return
+}
+
+// Seek sets the iterator's offset
+func (i *Iterator) Seek(offset int) {
+	i.offset = offset
+}
+
+// FastForward increments the iterator's offset
+func (i *Iterator) FastForward(delta int) {
+	i.offset += delta
+}
+
+// HasBytesLeft checks whether the slice has some bytes left
+func (i *Iterator) HasBytesLeft() bool {
+	return i.offset < len(i.bs)
+}
+
+// Offset returns the offset
+func (i *Iterator) Offset() int {
+	return i.offset
+}
+
+// Dump dumps the rest of the slice
+func (i *Iterator) Dump() (bs []byte) {
+	if !i.HasBytesLeft() {
+		return
+	}
+	bs = make([]byte, len(i.bs) - i.offset)
+	copy(bs, i.bs[i.offset:len(i.bs)])
+	i.offset = len(i.bs)
+	return
+}
---
+++ byte/iterator_test.go
@@ -0,0 +1,49 @@
+package astibyte
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIterator(t *testing.T) {
+	// Setup
+	i := NewIterator([]byte("1234567"))
+
+	// Next byte
+	b, err := i.NextByte()
+	assert.NoError(t, err)
+	assert.Equal(t, byte('1'), b)
+
+	// Next bytes
+	bs, err := i.NextBytes(3)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("234"), bs)
+	assert.Equal(t, 4, i.Offset())
+
+	// Fast forward
+	i.FastForward(2)
+	assert.Equal(t, 6, i.Offset())
+	assert.True(t, i.HasBytesLeft())
+
+	// Last byte
+	b, err = i.NextByte()
+	assert.NoError(t, err)
+	assert.Equal(t, byte('7'), b)
+	assert.False(t, i.HasBytesLeft())
+
+	// No bytes
+	b, err = i.NextByte()
+	assert.Error(t, err)
+
+	// Dump
+	i.FastForward(-2)
+	assert.Equal(t, []byte("67"), i.Dump())
+
+	// Seek
+	i.Seek(2)
+	b, err = i.NextByte()
+	assert.NoError(t, err)
+	assert.Equal(t, byte('3'), b)
+	assert.True(t, i.HasBytesLeft())
+}
