014281c0bf3a4ed1138c02a45045ceae68996853
Quentin Renard <contact@asticode.com>
2019-12-03 16:54:53 +0100

    Added int64 sorting

---
+++ sort/int64.go
@@ -0,0 +1,13 @@
+package astisort
+
+import "sort"
+
+// Int64 sorts a slice of int64s in increasing order.
+func Int64(a []int64) { sort.Sort(Int64Slice(a)) }
+
+// Int64Slice attaches the methods of Interface to []int64, sorting in increasing order.
+type Int64Slice []int64
+
+func (p Int64Slice) Len() int           { return len(p) }
+func (p Int64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p Int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
